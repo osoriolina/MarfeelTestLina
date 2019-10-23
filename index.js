0ec1b658818a453b4033bec1508f3f29e6d6486f
Lina <osoriolina@gmail.com>
2019-10-23 18:43:14 +0200

    Prueba Marfeel - falta limpiar los datos cuando se oprime de nuevo SEARCH

--- index.js
+++ index.js
@@ -2,43 +2,93 @@ function httpGet(theUrl, callback) {
 
     let xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function () {
-       
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
             callback(xmlHttp.responseText);
         else if (xmlHttp.readyState == 4)
             error(xmlHttp.responseText);
     }
     xmlHttp.open("GET", theUrl);
-
     xmlHttp.setRequestHeader("X-Requested-With", "XMLHttpRequest")
-    
     xmlHttp.send();
 }
 
+function error(err){ //pegar tercer que NO es un alert
+    limpiaPantalla();
+    document.getElementById("tablaTres").className = document.getElementById("tablaTres").className.replace(" d-none", "")
 
-function error(err){
-    alert("Not Found") 
-    
+}
+
+function limpiaPantalla () {
+    if(document.getElementById("tablaUno").className.indexOf("d-none") === -1) 
+        document.getElementById("tablaUno").className += " d-none"
+
+    if(document.getElementById("tablaUno").className.indexOf("d-none") === -1) 
+        document.getElementById("tablaDos").className += " d-none"
+
+    if(document.getElementById("tablaUno").className.indexOf("d-none") === -1) 
+        document.getElementById("tablaTres").className += " d-none"
 }
 
 
+function buscaUsuario(){
+    let user = document.querySelector("#input__usuario").value
+    httpGet("https://api.github.com/users/" + user, imprime)
+    
+} 
+
+function imprime(texto){ //callback
+    let usuario = JSON.parse(texto);
+    console.log(usuario)
 
 
-  function imprimir(texto){ //callback
-    let usuarioGit= JSON.parse(texto);
-    console.log(usuarioGit)
-  }
+    limpiaPantalla();
+    document.getElementById("tablaDos").className = document.getElementById("tablaDos").className.replace(" d-none", "")
+
+    let user = document.querySelector("#input__usuario").value
+    // let avatar = usuario.avatar_url
+    // document.querySelector("#imagen") = avatar
+
+    let imagen = document.querySelector("#imagenCarta");
+    imagen.src= usuario.avatar_url
+
+    let nombreLogin = usuario.login
+    document.querySelector("#username").innerHTML = nombreLogin
+
+    let nombreBio = usuario.bio
+    document.querySelector("#userBio").innerHTML = nombreBio
+
+    let nombreUsuario = usuario.name
+    document.querySelector("#nombreUser").innerHTML = nombreUsuario
+    
+    //console.log(usuario.bio)
+    //console.log(usuario.name)
+    //console.log(nombreLogin)
+
+    httpGet("https://api.github.com/users/" + user + "/repos", imprimeRepos)
+}
 
 
-let usuarioPrueba = document.getElementById("input__usuario").value
+function consultarDatos(repos) {
+
+    repos.forEach(  (repo) => {
+        let rowRepos = `<tr><th scope="row">${repo.name}</th><td><i class="fas fa-star"></i>${repo.watchers_count}<i class="fas fa-code-branch"></i>${repo.forks_count}</td></tr>`
+        document.querySelector("#datosRepos").innerHTML += rowRepos
+    });
+
+}
+
+function imprimeRepos(texto) {
+    let repos = JSON.parse(texto);
+    console.log(repos)
+    consultarDatos(repos)
+  
+}   
 
 
-let button = document.querySelector(".btn__user")
-document.querySelectorAll(".btn__user")[0].addEventListener('click', () => {
-    usuarioPrueba = document.getElementById("input__usuario").value;
-    httpGet("https://api.github.com/users/" + usuarioPrueba, function(response){
-    imprimir(response)
-})
+let arrBotonBuscar = document.querySelectorAll(".botonSearch")
+arrBotonBuscar.forEach( (botonBuscar) => {
+    botonBuscar.addEventListener("click", buscaUsuario);
+});
 
-    //httpPost("https://api.github.com/users?since=135", response)
-})
+
+
